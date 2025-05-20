b726c5802f8eef2cf9185e9678d9e74472f77a8f
giovanna <giovannafgdo@gmail.com>
2025-05-20 16:39:19 -0300

    feat: adiciona launch

--- slalon/setup.py
+++ slalon/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/slalon_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,8 +21,11 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'slalon_movement = slalon.slalon_movement:main',
-            'obstacle_detection = slalon.obstacle_detection:main',
+            'depth_st = slalon.depth_st:main',
+            'movement = slalon.movement:main',
         ],
+        'launch': [
+        'slalon_launch = slalon.launch.slalon_launch:generate_launch_description',
+    ],
     },
 )
