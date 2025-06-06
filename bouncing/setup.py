9c61c77e050f067ba69fba42678459f8d3a4774c
lipe-pedras <lfljp@hotmail.com>
2025-06-06 17:14:38 -0300

    fix (setup.py): fix bouncing_node path

--- bouncing/setup.py
+++ bouncing/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'bouncing_detector = bouncing.bouncing_node:main',
+            'bouncing_node = bouncing.bouncing_node:main',
         ],
     },
 )
